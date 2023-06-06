bf86787e1b37e8d43963425aa56aad37a8bde84f
Daniel Osorio <danioshi@gmail.com>
2023-06-06 10:04:38 +0200

    First commit

---
+++ ConnectOpenAI.py
@@ -0,0 +1,88 @@
+from typing import Tuple
+
+import openai
+import tiktoken
+import variables
+import time
+
+
+class ConnectOpenAI:
+    def __init__(self, api_key: str, **kwargs):
+        self.api_key = api_key
+        self.model = kwargs.get('model', 'gpt-4')
+        self.max_tokens = kwargs.get('max_tokens', 2048)
+        self.frequency_penalty = kwargs.get('frequency_penalty', 0.2)
+        self.presence_penalty = kwargs.get('presence_penalty', 0.2)
+        self.messages = [{"role": "system", "content": variables.system_prompt}]
+
+        self.estimated_tokens = 0
+        self.total_tokens = 0
+
+    def num_tokens_from_messages(self, messages, model="gpt-3.5-turbo-0301"):
+        """Returns the number of tokens used by a list of messages."""
+        try:
+            encoding = tiktoken.encoding_for_model(model)
+        except KeyError:
+            print("Warning: model not found. Using cl100k_base encoding.")
+            encoding = tiktoken.get_encoding("cl100k_base")
+        if model == "gpt-3.5-turbo":
+            print("Warning: gpt-3.5-turbo may change over time. Returning num tokens assuming gpt-3.5-turbo-0301.")
+            return self.num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301")
+        elif model == "gpt-4":
+            print("Warning: gpt-4 may change over time. Returning num tokens assuming gpt-4-0314.")
+            return self.num_tokens_from_messages(messages, model="gpt-4-0314")
+        elif model == "gpt-3.5-turbo-0301":
+            tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+            tokens_per_name = -1  # if there's a name, the role is omitted
+        elif model == "gpt-4-0314":
+            tokens_per_message = 3
+            tokens_per_name = 1
+        else:
+            raise NotImplementedError(
+                f"""num_tokens_from_messages() is not implemented for model {model}. 
+                See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are 
+                converted to tokens.""")
+        num_tokens = 0
+        for message in messages:
+            num_tokens += tokens_per_message
+            for key, value in message.items():
+                num_tokens += len(encoding.encode(value))
+                if key == "name":
+                    num_tokens += tokens_per_name
+        num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+        return num_tokens
+
+    def moderate_message(self, user_message: str, test: bool = False, test_flagged: bool = False) -> bool:
+        if test:
+            flagged = test_flagged
+        else:
+            openai.api_key = self.api_key
+            response = openai.Moderation.create(input=user_message)
+            flagged = response['results'][0]['flagged']
+        return flagged
+
+    def create_story(self, user_message: str, test: bool = False, test_reason: str = 'stop', wait_time: int = 1) -> \
+            Tuple[str, str]:
+        if test:
+            if not any(test_reason in reason for reason in ('stop', 'length', 'content_filter')):
+                test_reason = 'stop'
+            time.sleep(wait_time)
+            story = variables.example_story
+            finish_reason = test_reason
+        else:
+            self.messages.append({"role": "user", "content": user_message})
+            self.estimated_tokens = self.num_tokens_from_messages(self.messages, self.model)
+            openai.api_key = self.api_key
+            response = openai.ChatCompletion.create(
+                model=self.model,
+                messages=self.messages,
+                max_tokens=self.max_tokens,
+                frequency_penalty=self.frequency_penalty,
+                presence_penalty=self.presence_penalty,
+            )
+
+            story = response['choices'][0]['message']['content']
+            finish_reason = response['choices'][0]['finish_reason']
+            self.total_tokens = response['usage']['total_tokens']
+
+        return story, finish_reason
---
+++ main.py
@@ -0,0 +1,140 @@
+import ConnectOpenAI
+import streamlit as st
+from google.oauth2 import service_account
+from gsheetsdb import connect
+import variables
+
+# from st_files_connection import FilesConnection
+
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows_sheet = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows_sheet:
+    st.write(row)
+    # st.write(f"{row.name} has a :{row.pet}:")
+
+# Setting up the sidebar
+st.sidebar.header('How to use')
+st.sidebar.markdown(variables.sidebar_how_to_use)
+st.sidebar.divider()
+
+st.sidebar.header('FAQ')
+for element in variables.faq:
+    for type_text, text in element.items():
+        if type_text == 'question':
+            st.sidebar.subheader(text)
+        elif type_text == 'answer':
+            st.sidebar.write(text)
+
+st.title('Story Sprout')
+st.subheader('Create respectful stories for children aged 2-6 using OpenAI GPT-4')
+st.divider()
+
+connect_openai = ConnectOpenAI.ConnectOpenAI(api_key=st.secrets['OPENAI_KEY'])
+
+
+def create_prompt_section():
+    with st.expander(label='Prompt', expanded=True):
+        st.write(variables.prompt_language_text)
+        st.text_area('Short description', max_chars=400, key='user_message', help=variables.prompt_text_area_help,
+                     placeholder='Write a story about...')
+        st.number_input('Age of the child?', min_value=2, max_value=6, step=1, format='%d', key='age',
+                        help=variables.prompt_age_help)
+    st.button('Generate a story', on_click=check_prompt)
+    st.divider()
+
+
+def check_prompt():
+    """ Do all the checks here before preparing the story """
+    # Checking that the prompt is not empty
+    if not st.session_state.user_message:
+        prompt_error = variables.prompt_no_text_error
+    # If we have a prompt, moderate it
+    else:
+        # If the prompt is flagged, we show an error message
+        if not connect_openai.moderate_message(st.session_state.user_message, test=True, test_flagged=True):
+            prompt_error = variables.prompt_flagged_error
+        else:
+            prompt_error = False
+    st.session_state.prompt_error = prompt_error
+
+
+def generate_story():
+    user_message = f'{st.session_state.user_message}\n\nMake the story for a {st.session_state.age} year old.'
+    story_text, finish_reason = connect_openai.create_story(user_message=user_message, test=True, wait_time=4)
+    story_warning_text = None
+    match finish_reason:
+        case 'length':
+            story_warning_text = 'The response was cut off because it was too long.'
+        case 'content_filter':
+            story_warning_text = 'The story is not respecting OpenAI\'s usage policies.'
+    st.session_state.story_warning = story_warning_text
+    st.session_state.story = story_text
+
+
+def create_feedback_section():
+    st.write(variables.rate_msg)
+    st.session_state['feedback'] = st.radio('Help us by rating this story', range(6),
+                                            horizontal=True,
+                                            format_func=lambda x: variables.rate_options.get(x))
+    st.session_state['additional_comments'] = st.text_area("Additional comments:")
+    st.button("Submit Feedback", on_click=provide_feedback)
+
+
+def provide_feedback():
+    st.session_state.feedback_given = True
+
+
+def restart_app():
+    for key in st.session_state.keys():
+        del st.session_state[key]
+
+
+if 'story' not in st.session_state:
+    st.session_state.story = ''
+if 'prompt_error' not in st.session_state:
+    st.session_state.prompt_error = None
+if 'story_warning' not in st.session_state:
+    st.session_state.story_warning = None
+if 'feedback_given' not in st.session_state:
+    st.session_state.feedback_given = False
+
+create_prompt_section()
+if st.session_state.prompt_error is not None:
+    if st.session_state.prompt_error:
+        st.error(st.session_state.prompt_error)
+    else:
+        if not st.session_state.story:
+            with st.spinner('Creating your story, please be patient...'):
+                generate_story()
+                if st.session_state.story_warning:
+                    st.warning(st.session_state.story_warning)
+                st.success("Here's your story!")
+
+        st.write(st.session_state.story)
+        st.divider()
+        if not st.session_state.feedback_given:
+            create_feedback_section()
+        else:
+            st.info('Thank you for your feedback!')
+            st.button("Create another story?", on_click=restart_app)
